afa942ce8c9b8201488409292b5f42f2c6be81aa
sanghee <wjdtkdgml7352@daum.net>
2023-06-21 14:27:36 +0900

    first commit

--- test.js
+++ test.js
@@ -3,4 +3,13 @@ function b() {
     for(let i = 0; i<10; i++){}
     return i;
 }
-console.log(b())
+// console.log(b())
+
+function a() {
+    return 1
+}
+
+const a_ES6 = () => 1
+
+console.log(a())
+console.log(a_ES6())
---
+++ test2.js
@@ -0,0 +1,8 @@
+function a() {
+    return 1
+}
+
+const a_ES6 = () => 1
+
+console.log(a())
+console.log(a_ES6())
