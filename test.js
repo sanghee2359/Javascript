79eb8488503007897c22688b2f1176a6923aebbb
sanghee <wjdtkdgml7352@daum.net>
2023-06-22 20:55:11 +0900

    테스트 :  for}Each, map, filter, reduce, every, some

--- test.js
+++ test.js
@@ -32,3 +32,32 @@ function not_arrow() {
 
 const p1 = new not_arrow();
 const p2 = new arrow();
+
+const func1 = (e, index) => {
+    console.log(`${index}번째 요소는 ${e}입니다.`)
+}
+const func2 = (e, index) => e * 2
+const func3 = (prev, curr, index) => prev + curr
+const func4 = e => e % 2
+const d = [1, 2, 3, 4, 5].forEach(func1)
+
+/*
+0번째 요소는 1입니다.
+1번째 요소는 2입니다.
+2번째 요소는 3입니다.
+3번째 요소는 4입니다.
+4번째 요소는 5입니다.
+*/
+
+console.log(d) // undefined
+const e = [1,2,3,4,5].map(func2)
+console.log(e)  // [2, 4, 6, 8, 10]
+const f = [1,2,3,4,5].filter(func4)
+console.log(f) // [1, 3, 5]
+const g = [1,2,3,4,5].reduce(func3)
+console.log(g)
+
+const numbers = [1, 3, 5, 4]
+const isAllOdd = numbers.every(e => e %2)
+const isSomeOdd = numbers.some(e => e %2)
+console.log(isAllOdd, isSomeOdd)
